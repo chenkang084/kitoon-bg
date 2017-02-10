0135444374da062699c20d5318726124b78fd4b1
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-02-10 15:31:24 +0800

    add mongo

--- app.js
+++ app.js
@@ -7,8 +7,24 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var settings = require('./settings');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
+
+
+app.use(session({
+    secret: settings.cookieSecret,
+    key: settings.db, //cookie name
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }, //30 days
+    store: new MongoStore({
+        db: settings.db,
+        host: settings.host,
+        port: settings.port
+    })
+}));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,20 +43,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
