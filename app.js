ecd0631f945a0d38b2152cff9f9129c8d05cf093
chenkang1 <chenkang1@lenovo.com>
2017-03-12 20:12:33 +0800

    add wechat bg

--- app.js
+++ app.js
@@ -1,18 +1,69 @@
+var wechat = require('wechat');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var index = require('./routes/index');
-var users = require('./routes/users');
+let routers = require('./routes/modules.js');
 var settings = require('./settings');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-let routers = require('./routes/modules.js');
+
+var config = {
+  token: 'mygame',
+  appid: 'wxdee062ba0c93c45b',
+  encodingAESKey: 'hxlqJhRy5fVRU2xcl4dyJ0qC8MaUc5NgUCnlvEAVUes',
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+};
 
 var app = express();
 routers(app);
+
+app.use(express.query());
+app.use('/token', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+//   if (message.FromUserName === 'diaosi') {
+//     // 回复屌丝(普通回复)
+//     res.reply('hehe');
+//   } else if (message.FromUserName === 'text') {
+//     //你也可以这样回复text类型的信息
+//     res.reply({
+//       content: 'text object',
+//       type: 'text'
+//     });
+//   } else if (message.FromUserName === 'hehe') {
+//     // 回复一段音乐
+//     res.reply({
+//       type: "music",
+//       content: {
+//         title: "来段音乐吧",
+//         description: "一无所有",
+//         musicUrl: "http://mp3.com/xx.mp3",
+//         hqMusicUrl: "http://mp3.com/xx.mp3",
+//         thumbMediaId: "thisThumbMediaId"
+//       }
+//     });
+//   } else {
+//     // 回复高富帅(图文回复)
+//     res.reply([
+//       {
+//         title: '你来我家接我吧',
+//         description: '这是女神与高富帅之间的对话',
+//         picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+//         url: 'http://nodeapi.cloudfoundry.com/'
+//       }
+//     ]);
+//   }
+
+    var arr = ['你好，欢迎光临！','hello Li Longlong','我现在只会随便说说...','waiting, i am developing ...']
+
+    var index = Math.floor((Math.random()*arr.length));
+
+    res.reply(arr[index]);
+}));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,7 +86,6 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
--- routes/modules.js
+++ routes/modules.js
@@ -1,5 +1,7 @@
-let routers = function routers(app) {
+const token = require('./token');
+var xmlparser = require('express-xml-bodyparser');
 
+let routers = function routers(app) {
     //allow custom header and CORS
     app.all('*', function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
@@ -14,11 +16,8 @@ let routers = function routers(app) {
         }
     });
 
-
-
-
     app.get('/', function(req, res, next) {
-        res.render('index', { title: 'Express' });
+        res.render('index', { title: 'Express',date:new Date()});
     });
 
     app.get('/getData', function(req, res, next) {
@@ -31,12 +30,26 @@ let routers = function routers(app) {
     });
 
     app.get('/api/v1/users/me', function(req, res, next) {
-        res.send({"username":"admin","email":"admin@localhost","role":"admin","groups":[],"type":0,"status":true,"firstname":"","lastname":"","notificationenabled":false});
+        res.send({ "username": "admin", "email": "admin@localhost", "role": "admin", "groups": [], "type": 0, "status": true, "firstname": "", "lastname": "", "notificationenabled": false });
     });
 
+    // app.get('/token', function(req, res, next) {
+    //     token(req, res);
+    // });
 
-
-
+    // app.post('/token', xmlparser({ trim: false, explicitArray: false }), function(req, res) {
+    //     console.log(req.body);
+    //     data = '<xml>'+
+    //                 '<ToUserName><![CDATA['+req.body.tousername+']]></ToUserName>'+
+    //                 '<FromUserName><![CDATA[${req.fromusername}]]></FromUserName>'+
+    //                 '<CreateTime>1348831860</CreateTime>'+
+    //                 '<MsgType><![CDATA[text]]></MsgType>'+
+    //                 '<Content><![CDATA[this is a test]]></Content>'+
+    //                 '<MsgId>1234567890123456</MsgId>'+
+    //             '</xml>';
+    //     res.writeHead(200, { 'Content-Type': 'application/xml' });
+    //     res.end(data);
+    // })
 }
 
 module.exports = routers;
